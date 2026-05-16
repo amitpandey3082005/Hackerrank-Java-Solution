55e208a0ddf132b631f9617499e666036695c031
Amit Pandey <amitpandey1187170@gmail.com>
2026-05-16 00:01:54 -0700

    Update OutputFormatting.java

--- Java-Basics/OutputFormatting.java
+++ Java-Basics/OutputFormatting.java
@@ -1,1 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s = sc.next();
+            int x = sc.nextInt();
+            
+            System.out.printf("%-15s%03d\n", s , x);
+        }
+        System.out.println("================================");
+    }
+}
 
