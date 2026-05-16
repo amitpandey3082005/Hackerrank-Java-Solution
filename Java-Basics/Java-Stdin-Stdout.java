1b59f915546ad8bc69a45e9b88ec64aeec51ff83
Amit Pandey <amitpandey1187170@gmail.com>
2026-05-16 00:09:18 -0700

    Update Java-Stdin-Stdout.java

--- Java-Basics/Java-Stdin-Stdout.java
+++ Java-Basics/Java-Stdin-Stdout.java
@@ -1,1 +1,19 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double d = sc.nextDouble();
+          sc.nextLine(); // consume left over new line charecter 
+        String str = sc.nextLine();
+        
+        System.out.println("String: "+str);
+        System.out.println("Double: "+d);
+        System.out.println("Int: "+n);
+    
+    }
+}
